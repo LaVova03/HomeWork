7952526069b1090ce37f4e912f11bde7d6fe9bd3
Vladimir Lalabekyan <davidvovchik@gmail.com>
2023-05-06 13:32:42 +0300

    update commit

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ town ? town : alert('Шкода, що Ви не захотіли ввести с
 const favoriteSport = prompt("Який ваш улюблений вид спорту; 'Футбол', 'Бокс', 'Баскетбол'?");
 favoriteSport ? favoriteSport : alert('Шкода, що Ви не захотіли ввести свій улюблений вид спорту');
 
-const newAge = age ? `${'Вам'}  ${2023 - age}  ${'роки(ів)'}` : false;
+const newAge = age ? `${'Вам'}  ${2023 - age}  ${'роки(ів)'}` : 'Помилка';
 
 const newTown = town.toUpperCase();
 switch (newTown) {
-    case '': break;
+    case '':
+        alert(`${newAge}`);
+        break;
     case 'КИЇВ':
         alert(`${newAge} ${'Ви проживаєте у столиці України'}`);
         break;
